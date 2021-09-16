74fc9f143c1ea6166f4cddf34c79ac6f031b621a
Kyle Mayer <x@kylemayer.dev>
2021-09-16 10:08:25 -0700

    file layout and Hey Arnold API fetch request file

---
+++ src/services/heyArnoldApi.js
@@ -0,0 +1,12 @@
+export const fetchCharacters = async () => {
+  const res = await fetch(
+    'https://hey-arnold-api.herokuapp.com/api/v1/characters'
+  );
+  const characters = await res.json();
+
+  return characters.map((character) => ({
+    id: character._id,
+    name: character.name,
+    image: character.image,
+  }));
+};
